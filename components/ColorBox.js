335bf26b628f1f8eeeb6df872a5fffde75d2f7c8
jenross <ross.m.jennifer@gmail.com>
2020-03-06 15:13:59 -0500

    navigation exercise

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -31,6 +31,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
   },
 });
 
